0139e31834c80b46a42d316d6f46603048d3104c
Pranjal-PSD <pranjaldhomane@gmail.com>
2025-07-24 16:33:37 +0530

    Update stage_sales_data.sql

--- dev/stage_sales_data.sql
+++ dev/stage_sales_data.sql
@@ -1,1 +1,5 @@
 -- Stage sales data SQL 
+INSERT INTO sales_data (sale_id, product, amount, sale_date) VALUES
+(1, 'Product A', 100.50, '2025-07-01'),
+(2, 'Product B', 250.00, '2025-07-02'),
+(3, 'Product A', 75.25, '2025-07-03');
